a6fbddd452ba8f0db30dcc08c21aa23b803601fa
Misskhat <misskhat.hossen@gmail.com>
2025-12-22 23:40:54 +0600

    baseURL update

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://sh-deco-server.vercel.app"
 })
 
 const useAxios = () => {
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth';
 import { useNavigate } from 'react-router';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://sh-deco-server.vercel.app"
 })
 
 const useAxiosSecure = () => {
